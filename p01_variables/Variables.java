a43c5dd01799b1ba7b3c1040d98cce71a2b92285
Rodolfo Mora <rodolfomora97@outlook.com>
2026-02-05 10:46:27 -0600

    initial commit

---
+++ p01_variables/Variables.java
@@ -0,0 +1,15 @@
+
+
+package p01_variables;
+
+/**
+ *
+ * @author Rodolfo
+ */
+public class Variables {
+    
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+    
+}
